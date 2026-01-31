35c9f7d14bca0c4c22157020136f8aaf65dd5b8c
Levi Sherrow <levisherrow@usd204.net>
2026-01-31 09:00:26 -0600

    I ran out of commit message ideas ]=

--- commands/climberClimb.py
+++ commands/climberClimb.py
@@ -3,23 +3,24 @@ Copy this file and write your own commands based on it. This file should not be 
 """
 
 from commands2 import Command
-from subsystems.climber import climber
+from subsystems.climber import Climber
 
 class ClimberClimb(Command):
-    def __init__(self, Climber: climber):
+    def __init__(self, climber: Climber):
         super().__init__()
-        self.addRequirements(self)
-        self.climber=Climber
+        self.climber = climber
+        self.addRequirements(climber)
         # Use addRequirements() here to declare subsystem dependencies.
         # e.g. self.addRequirements(subsystem)
 
     def initialize(self):
         pass
 
-    def execute(self):
-        pass
+    def execute(self,number):
+        self.climber.setVelocity(number)
 
     def end(self, interrupted: bool):
+        self.climber.setVelocity(0)
         pass
 
     def isFinished(self) -> bool:
--- subsystems/climber.py
+++ subsystems/climber.py
@@ -16,38 +16,61 @@ import commands2
 from phoenix6.hardware import TalonFX
 from wpimath.units import inches
 
-class climber(commands2.Subsystem):
+class Climber(commands2.Subsystem):
 
-  def __init__(self,side):
+  def __init__(self):
     #def motors
     #def uhh... other stuff
-    self.Elevator=TalonFX(side)
-    self.positionMeWanty=0
+    self.LElevator=TalonFX(1)
+    self.RElevator=TalonFX(2)
+    self.LWanty=0
+    self.RWanty=0
     self.velocity=1
 
   def periodic(self):
-    self.Elevator.set(self.velocity)
+#    Lheight=self.getCurrentPosition("L")*3.14*3
+#    if Lheight>self.getDesiredPosition("L"):
+    self.LElevator.set(self.velocity)
   
   def simulationPeriodic(self) -> None:
     pass
   
-  def getCurrentPosition(self) -> inches:
-    return self.Elevator.get_position().value_as_double
+  def getCurrentPosition(self,side) -> inches:
+    if side==("L"):
+      return self.LElevator.get_position().value_as_double
+    elif side==("R"):
+      return self.RElevator.get_position().value_as_double
+    else:
+      self.error
 
   def setCurrentPosition(self) -> None:
     pass
   
-  def getDesiredPosition(self) -> inches:
-    return self.positionMeWanty
+  def getDesiredPosition(self,side) -> inches:
+    if side==("L"):
+      return self.LWanty
+    elif side==("R"):
+      return self.RWanty
+    else:
+      self.error
   
-  def setDesiredPosition(self) -> None:
-    pass
+  def setDesiredPosition(self,position,side) -> None:
+    if side==("L"):
+      self.LWanty=(position)
+    elif side==("R"):
+      self.RWanty=(position)
+    else:
+      self.error
+
   
   def atPosition(self) -> bool:
     return ( self.getCurrentPosition() == self.getDesiredPosition() )
   
   def setVelocity(self,V):
     self.velocity = V
+
+  def error(self):
+    print ("I don't know what side that's pertaining to! use L or R when specifying which elevator.")
 
 """
     ||    ||
